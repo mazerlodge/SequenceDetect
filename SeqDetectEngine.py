a539df76d1f52c7488f061002cb40388914b2e9a
mazerlodge <mazerlodge@hotmail.com>
2024-03-02 18:38:40 -0600

    Initial version done. - Fleshed out methods - added support for -debug command line parameter

--- SeqDetectEngine.py
+++ SeqDetectEngine.py
@@ -3,13 +3,16 @@ import sys
 class SeqDetectEngine: 
 
 	bInitOK = False
-	bInDebug = True
+	bInDebug = False
 	rawNumber = 0
 
 	def __init__(self, args): 
 		self.parseArgs(args)
 
+
 	def parseArgs(self, args):
+		self.bInitOK = False 
+
 		argCount = len(args)
 		self.debugMsg(f"Arg Count = {argCount}") 
 		
@@ -17,41 +20,63 @@ class SeqDetectEngine:
 			self.rawNumber = int(args[1])
 			self.bInitOK = True
 
-			# Debug use only
-			if (self.bInDebug):
-				for x in range(0,argCount):
-					self.debugMsg(f"arg[{x}] = {args[x]}")
+		if ((argCount >= 3) and (args[2].lower() == "-debug")):
+			self.bInDebug = True
+				
+		if (self.bInDebug):
+			for x in range(0,argCount):
+				self.debugMsg(f"arg[{x}] = {args[x]}")
 
-		else:
-			self.bInitOK = False 
 
 	def showUsage(self):
-		print("Usage: SeqDetect 456 (returns True).\nSeqDetect 457 (returns False)")
+		print("Usage: SeqDetect 456 [-debug] (returns True).\nSeqDetect 457 (returns False)")
+	
 	
 	def countDigits(self, inNum):
 		return len(str(inNum))
 		
-	def isSequence(self, inNum): 
-		numLength = self.countDigits(self.rawNumber)
-		print(f"Your number {self.rawNumber} has {numLength} digits")
-		digitArray = []
+		
+	def makeArrayOfDigits(self, fromNum):
+		digitArray = []	
+		numLength = self.countDigits(fromNum)
 
+		self.debugMsg(f"Your number {fromNum} has {numLength} digits")
 
-		workingNum = self.rawNumber
+		# put the digits of the input number into an array 
+		workingNum = fromNum
 		for currentDigIdx in range(numLength-1, 0, -1):
 			denom = 10 ** currentDigIdx
 			currentDigit = workingNum // denom 
 			digitArray.append(currentDigit)
-			workingNum = workingNum - (currentDigit * denom) 
+			workingNum = workingNum - (currentDigit * denom) 	
 
+		# add the last digit to the array
 		digitArray.append(workingNum)
 
-		for aDig in digitArray:
-				print(aDig)
+		return(digitArray)
+		
+		
+	def isSequence(self, inNum): 
+		bIsSequence = True
+		digitArray = self.makeArrayOfDigits(inNum)
+
+		# Walk digits, right to left, seeing if the val is 1 less than prev val
+		adjustedVal = digitArray[len(digitArray)-1]
+		for x in range(len(digitArray)-1, -1, -1):
+			if (digitArray[x] != adjustedVal):
+				bIsSequence = False
+				break	
+			adjustedVal = adjustedVal - 1	
+			
+		return(bIsSequence)
+
 
 	def go(self):
-		self.isSequence(self.rawNumber)
-
+		if (self.isSequence(self.rawNumber)):
+			print(f"Your number {self.rawNumber} is a sequence.")
+		else:
+			print(f"Your number {self.rawNumber} is not a sequence.")
+		
 			
 
 	def debugMsg(self, msg): 
