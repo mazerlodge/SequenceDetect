b0ec76098b7b0e70d825fa70320f97690f23c8d4
mazerlodge <mazerlodge@hotmail.com>
2024-05-24 11:20:16 -0500

    20240524-1120 Updates. - Added shell script for command line exec via: ./seqcheck.sh 456 - Added isOutOfOrderSequence() and call in SDE.go()

--- SeqDetectEngine.py
+++ SeqDetectEngine.py
@@ -15,14 +15,14 @@ class SeqDetectEngine:
 
 		argCount = len(args)
 		self.debugMsg(f"Arg Count = {argCount}") 
-		
+
 		if (argCount >= 2):
 			self.rawNumber = int(args[1])
 			self.bInitOK = True
 
 		if ((argCount >= 3) and (args[2].lower() == "-debug")):
 			self.bInDebug = True
-				
+
 		if (self.bInDebug):
 			for x in range(0,argCount):
 				self.debugMsg(f"arg[{x}] = {args[x]}")
@@ -30,14 +30,14 @@ class SeqDetectEngine:
 
 	def showUsage(self):
 		print("Usage: SeqDetect 456 [-debug] (returns True).\nSeqDetect 457 (returns False)")
-	
-	
+
+
 	def countDigits(self, inNum):
 		return len(str(inNum))
-		
-		
+
+
 	def makeArrayOfDigits(self, fromNum):
-		digitArray = []	
+		digitArray = []
 		numLength = self.countDigits(fromNum)
 
 		self.debugMsg(f"Your number {fromNum} has {numLength} digits")
@@ -48,14 +48,14 @@ class SeqDetectEngine:
 			denom = 10 ** currentDigIdx
 			currentDigit = workingNum // denom 
 			digitArray.append(currentDigit)
-			workingNum = workingNum - (currentDigit * denom) 	
+			workingNum = workingNum - (currentDigit * denom) 
 
 		# add the last digit to the array
 		digitArray.append(workingNum)
 
 		return(digitArray)
-		
-		
+
+
 	def isSequence(self, inNum): 
 		bIsSequence = True
 		digitArray = self.makeArrayOfDigits(inNum)
@@ -65,9 +65,27 @@ class SeqDetectEngine:
 		for x in range(len(digitArray)-1, -1, -1):
 			if (digitArray[x] != adjustedVal):
 				bIsSequence = False
-				break	
-			adjustedVal = adjustedVal - 1	
-			
+				break
+			adjustedVal = adjustedVal - 1
+
+		return(bIsSequence)
+
+
+	def isOutOfOrderSequence(self, inNum): 
+		bIsSequence = True
+
+		# Sort the numbers 
+		digitArray = self.makeArrayOfDigits(inNum)
+		digitArray.sort()
+
+		# Walk digits, right to left, seeing if the val is 1 less than prev val
+		adjustedVal = digitArray[len(digitArray)-1]
+		for x in range(len(digitArray)-1, -1, -1):
+			if (digitArray[x] != adjustedVal):
+				bIsSequence = False
+				break
+			adjustedVal = adjustedVal - 1
+
 		return(bIsSequence)
 
 
@@ -75,11 +93,13 @@ class SeqDetectEngine:
 		if (self.isSequence(self.rawNumber)):
 			print(f"Your number {self.rawNumber} is a sequence.")
 		else:
-			print(f"Your number {self.rawNumber} is not a sequence.")
-		
-			
+			if (self.isOutOfOrderSequence(self.rawNumber)):
+				print(f"Your number {self.rawNumber} is an out of order sequence.")
+			else:
+				print(f"Your number {self.rawNumber} is not a sequence.")
+
 
 	def debugMsg(self, msg): 
 		if (self.bInDebug):
 			print(msg)
-	
+
